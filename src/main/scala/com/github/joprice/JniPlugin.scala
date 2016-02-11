9e590623c11ba503c2ce9c801a653466ebeaf882
Maksim Kanev <maksim.kanev@waveaccess.ru>
2016-02-11 14:39:40 +0300

    Legacy code removed

--- src/main/scala/com/github/joprice/JniPlugin.scala
+++ src/main/scala/com/github/joprice/JniPlugin.scala
@@ -106,11 +106,7 @@ object JniPlugin extends AutoPlugin { self =>
     jniSourceFiles := withExtensions(jniNativeSources.value, jniCppExtensions.value),
     jniCompile := Def.task {
       val log = streams.value.log
-//      new java.io.File(jniBinPath.value.getAbsolutePath).mkdirs()
       jniBinPath.value.getAbsoluteFile.mkdirs()
-      //      val mkBinDir = s"mkdir -p ${jniBinPath.value}"
-      //      log.info(mkBinDir)
-      //      mkBinDir ! log
       val sources = jniSourceFiles.value.mkString(" ")
       val flags = jniGccFlags.value.mkString(" ")
       val command = s"${jniNativeCompiler.value} $flags -o ${jniBinPath.value}/lib${jniLibraryName.value}.${jniLibSuffix.value} $sources"
